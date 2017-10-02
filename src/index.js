638d586e0e2c0dc817cd3e366486d645e82e3ae3
Lobov Konstantin <kos33rd@gmail.com>
2017-10-02 10:59:03 +0300

    webpack externals resolution

--- src/index.js
+++ src/index.js
@@ -15,25 +15,55 @@ function MintPlugin(options) {
     this.mask = options.mask || ['**/*.js']
 }
 
-MintPlugin.prototype.apply = function(compiler) {
+MintPlugin.prototype.apply = function (compiler) {
     const root = this.root
-    const projectFiles = getProjectFiles(root, this.mask)
-    compiler.plugin('compilation', function(compilation, params) {
-        compilation.plugin('after-optimize-chunk-assets', function(chunks) {
+    let projectFiles = getProjectFiles(root, this.mask)
+
+    if (compiler.options.externals) {
+        const ext = compiler.options.externals
+
+        _.remove(projectFiles, (file) => {
+            // fixme: manually converting file paths to webpack import format
+            let fileName = file
+            const fileExtension = '.'+_.last(file.split('.'))
+            if (_.includes(_.get(compiler, 'options.resolve.extensions', []), fileExtension)) {
+                fileName = file.split('.')[0]
+            }
+            const webpackedPath = './' + path.relative(path.dirname(root), fileName)
+                .replace(new RegExp('\\' + path.sep, 'g'), '/')
+            // end of fixme
+
+            let isExternal = true
+            if (typeof ext === "string") {
+                isExternal = (ext === webpackedPath)
+            } else if (Array.isArray(ext)) {
+                isExternal = _.includes(ext, webpackedPath)
+            } else if (ext instanceof RegExp) {
+                isExternal = ext.test(webpackedPath)
+            } else if (typeof ext === 'function') {
+                isExternal = ext(null, webpackedPath, (c, v) => v)
+            }
+            debug(`${webpackedPath} is treated as ${isExternal ? 'external' : 'internal'}`)
+            return isExternal
+        })
+    }
+
+    compiler.plugin('compilation', function (compilation, params) {
+        compilation.plugin('after-optimize-chunk-assets', function (chunks) {
 
             debug('Using files:')
-            _.map(compilation.fileDependencies, (f)=>{
+            _.map(compilation.fileDependencies, (f) => {
                 debug(`\t* ${f}`)
             })
             debug('-----')
 
             const unusedFiles = _.difference(projectFiles, compilation.fileDependencies)
-            if(!_.isEmpty(unusedFiles)){
-                console.log('[Mint] Found unused files:')
-                _.map(unusedFiles, (f)=>{
-                    console.log(`\t* ${f}`)
+            if (!_.isEmpty(unusedFiles)) {
+                let report = '[ Mint ]\nFound unused files:\n'
+                _.map(unusedFiles, (f) => {
+                    report += `\t* ${f}\n`
                 })
-                console.log('')
+                compilation.warnings.push(report)
             }
         })
     })
